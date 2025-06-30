179159ee3b3478eeb45a5a9abdb93061ea7c0778
sbrptdev2 <181986753+sbrptdev2@users.noreply.github.com>
2025-06-30 21:30:51 +0200

    Fixed compilation for 4.9.37

--- hal_apollo/main.c
+++ hal_apollo/main.c
@@ -1877,7 +1877,7 @@ int access_file(char *path, char *buffer, int size, int isRead)
 #endif
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 0))
 			ret = kernel_read(fp,buffer,size,&fp->f_pos);
-#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 84))
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 37))
 			ret = kernel_read(fp,fp->f_pos,buffer,size);
 
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 14))
@@ -1903,9 +1903,8 @@ int access_file(char *path, char *buffer, int size, int isRead)
 #endif
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 19, 0))
 			ret = kernel_write(fp,buffer,size,&fp->f_pos);
-#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 84))
+#elif (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 37))
 			ret = kernel_write(fp,buffer,size,fp->f_pos);
-
 #elif (LINUX_VERSION_CODE >= KERNEL_VERSION(4, 9, 14))
 			ret = kernel_write(fp,&fp->f_pos,buffer,size);
 #else
