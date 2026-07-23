2e8e2f330749c13d93f53a4e1a36fe4a95d060fc
helgard-orlm <helgard.orlm@gmail.com>
2026-07-23 16:26:10 +0300

    api: MODULE_IMPORT_NS() takes a string literal since 6.13

--- hal_apollo/main.c
+++ hal_apollo/main.c
@@ -64,7 +64,12 @@
 
 #if (LINUX_VERSION_CODE >= KERNEL_VERSION(5,10,0))
 
-MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver); 
+#if (LINUX_VERSION_CODE >= KERNEL_VERSION(6, 13, 0))
+/* порт на 6.18: MODULE_IMPORT_NS теперь требует строковый литерал */
+MODULE_IMPORT_NS("VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver");
+#else
+MODULE_IMPORT_NS(VFS_internal_I_am_really_a_filesystem_and_am_NOT_a_driver);
+#endif 
 #endif
 
 MODULE_AUTHOR("wifi_software <wifi_software@altobeam.com>");
